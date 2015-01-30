4466bc9f67493187566bdf817dc5b20392d2e18d
Pranay Shah <Pranay@Pranays-MacBook-Pro.local>
2015-01-29 21:22:49 -0800

    Initial Commit

---
+++ alertview/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  alertview
+//
+//  Created by Pranay Shah on 1/29/15.
+//  Copyright (c) 2015 Pranay Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
